a12a61063ec559d5068421a39113f2bba3b155ff
mahendrasingh250107 <53169458+mahendrasingh250107@users.noreply.github.com>
2019-08-15 13:26:40 +0200

    Update basinhopping.py

--- basinhopping.py
+++ basinhopping.py
@@ -17,6 +17,6 @@ def fun(x):
 # optimization process
 x_start = np.array ([- 5.0, 12.0])
 minimizer_kwargs = {"method": "L-BFGS-B","jac":True}
-result = basinhopping(fun, x0, niter=500, T=1.0, stepsize=0.5, minimizer_kwargs=minimizer_kwargs,interval=100, disp=True,niter_success=None)
+result = basinhopping(fun, x_start, niter=500, T=1.0, stepsize=0.5, minimizer_kwargs=minimizer_kwargs,interval=100, disp=True,niter_success=None)
 print("global minimum: x = [%.4f, %.4f], f(x) = %.4f" % (result.x[0],result.x[1],result.fun))
 
